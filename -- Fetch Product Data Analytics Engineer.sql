0937d0376b2f95467fe2cea11d47b45c07aebfc8
Habeeb Abdulrasaq <50907012+halbeeb@users.noreply.github.com>
2024-03-21 15:20:58 +0100

    Create Warehouse, Database, Schemas, and File Format for JSON

--- -- Fetch Product Data Analytics Engineer.sql
+++ -- Fetch Product Data Analytics Engineer.sql
@@ -6,3 +6,18 @@ CREATE WAREHOUSE IF NOT EXISTS Fetch_Rewards
     AUTO_RESUME = TRUE
     INITIALLY_SUSPENDED = TRUE;
 
+--Create database name products
+CREATE DATABASE IF NOT EXISTS Products;
+
+-- creating schema for the products database
+CREATE SCHEMA IF NOT EXISTS Products.fetch;
+
+-- Create File Format for JSON files
+CREATE OR REPLACE FILE FORMAT Products.fetch.json_format
+    TYPE = 'JSON'
+    STRIP_OUTER_ARRAY = TRUE
+    STRIP_NULL_VALUES = TRUE
+    IGNORE_UTF8_ERRORS = TRUE;
+
+
+
