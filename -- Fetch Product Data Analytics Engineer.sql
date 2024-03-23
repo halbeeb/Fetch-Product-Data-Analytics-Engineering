6f4091b2b977bbe09c244decd8210999984e84f5
Habeeb Abdulrasaq <50907012+halbeeb@users.noreply.github.com>
2024-03-23 13:53:15 +0100

    Exploring JSON data file and structure

--- -- Fetch Product Data Analytics Engineer.sql
+++ -- Fetch Product Data Analytics Engineer.sql
@@ -1,49 +1,37 @@
--- Fetch Product Data Analytics Engineering
---Creating warehouse, database and schemas
+-- Creating warehouse, database, and schema
 CREATE WAREHOUSE IF NOT EXISTS Fetch_Rewards
     WITH WAREHOUSE_SIZE = 'X-SMALL'
     AUTO_SUSPEND = 300
     AUTO_RESUME = TRUE
     INITIALLY_SUSPENDED = TRUE;
 
---Create database name products
 CREATE DATABASE IF NOT EXISTS Products;
 
--- creating schema for the products database
 CREATE SCHEMA IF NOT EXISTS Products.fetch;
 
--- Create File Format for JSON files
+-- Create a single file format for JSON files, assuming all files are GZIP compressed and in JSON format
 CREATE OR REPLACE FILE FORMAT Products.fetch.json_format
     TYPE = 'JSON'
     STRIP_OUTER_ARRAY = TRUE
     STRIP_NULL_VALUES = TRUE
     IGNORE_UTF8_ERRORS = TRUE;
 
+-- Create stages for uploaded files
+CREATE OR REPLACE STAGE Products.fetch.brands_stage
+    URL = 's3://habeebanalytics/brands.json'
+    FILE_FORMAT = (FORMAT_NAME = Products.fetch.json_format);
+
+CREATE OR REPLACE STAGE Products.fetch.users_stage
+    URL = 's3://habeebanalytics/users.json'
+    FILE_FORMAT = (FORMAT_NAME = Products.fetch.json_format);
+
 CREATE OR REPLACE STAGE Products.fetch.receipts_stage
     URL = 's3://habeebanalytics/receipts.json'
     FILE_FORMAT = (FORMAT_NAME = Products.fetch.json_format);
 
-LIST @Products.fetch.receipts_stage;
+Select * from @products.fetch.receipts_stage (file_format=> 'Products.fetch.json_format');
 
-
-
--- Create Stages for Uploaded Files
-CREATE OR REPLACE STAGE Products.fetch.brands_stage
-    URL = 's3://fetch-hiring/analytics-engineer/ineeddata-data-modeling/brands.json.gz'
-    FILE_FORMAT = (FORMAT_NAME = Products.fetch.json_format);
-
-CREATE OR REPLACE STAGE Products.fetch.users_stage
-    URL = 's3://fetch-hiring/analytics-engineer/ineeddata-data-modeling/users.json.gz'
-    FILE_FORMAT = (FORMAT_NAME = Products.fetch.json_format);
-
-CREATE OR REPLACE STAGE Products.fetch.receipts_stage
-    URL = 's3://fetch-hiring/analytics-engineer/ineeddata-data-modeling/receipts.json.gz'
-    FILE_FORMAT = (FORMAT_NAME = Products.fetch.json_format);
-
-describe Products.fetch.brands_stage;
-
--- Create tables for our data
--- Receipts Table
+-- Create tables for our data: Receipts, Users, Brands
 CREATE TABLE IF NOT EXISTS Products.fetch.receipts (
     _id VARCHAR,
     bonusPointsEarned FLOAT,
@@ -62,7 +50,6 @@ CREATE TABLE IF NOT EXISTS Products.fetch.receipts (
     userId VARCHAR
 );
 
--- Users Table
 CREATE TABLE IF NOT EXISTS Products.fetch.users (
     _id VARCHAR,
     state VARCHAR,
@@ -72,7 +59,6 @@ CREATE TABLE IF NOT EXISTS Products.fetch.users (
     active BOOLEAN
 );
 
--- Brand Table
 CREATE TABLE IF NOT EXISTS Products.fetch.brands (
     _id VARCHAR,
     barcode VARCHAR,
@@ -84,289 +70,77 @@ CREATE TABLE IF NOT EXISTS Products.fetch.brands (
     name VARCHAR
 );
 
--- Copy Data into Tables
-COPY INTO Products.fetch.brands
-FROM @Products.fetch.brands_stage
-FILE_FORMAT = (FORMAT_NAME = Products.fetch.json_format);
-
-COPY INTO Products.fetch.users
-FROM @Products.fetch.users_stage
-FILE_FORMAT = (FORMAT_NAME = Products.fetch.json_format);
-
-COPY INTO Products.fetch.receipts
-FROM @Products.fetch.receipts_stage
-FILE_FORMAT = (FORMAT_NAME = Products.fetch.json_format);
-
-
--- -- Copy Data into Tables
+-- Copy data into tables with match by column name case sensitive, error handling as required
 COPY INTO Products.fetch.brands
 FROM @Products.fetch.brands_stage
 FILE_FORMAT = (FORMAT_NAME = Products.fetch.json_format)
-MATCH_BY_COLUMN_NAME=CASE_SENSITIVE;
+MATCH_BY_COLUMN_NAME=CASE_SENSITIVE
+ON_ERROR = 'CONTINUE';
 
 COPY INTO Products.fetch.users
 FROM @Products.fetch.users_stage
 FILE_FORMAT = (FORMAT_NAME = Products.fetch.json_format)
-MATCH_BY_COLUMN_NAME=CASE_SENSITIVE;
+MATCH_BY_COLUMN_NAME=CASE_SENSITIVE
+ON_ERROR = 'CONTINUE';
 
 COPY INTO Products.fetch.receipts
 FROM @Products.fetch.receipts_stage
 FILE_FORMAT = (FORMAT_NAME = Products.fetch.json_format)
-MATCH_BY_COLUMN_NAME=CASE_SENSITIVE;
-
-
--- Testing the
-
-CREATE OR REPLACE FILE FORMAT Products.fetch.json_format
-    TYPE = 'JSON'
-    COMPRESSION = 'GZIP'
-    STRIP_OUTER_ARRAY = TRUE
-    STRIP_NULL_VALUES = TRUE
-    IGNORE_UTF8_ERRORS = TRUE;
-
-
-CREATE OR REPLACE STAGE Products.fetch.receipts_stage
-    URL = 's3://fetch-hiring/analytics-engineer/ineeddata-data-modeling/'
-    FILE_FORMAT = (FORMAT_NAME = Products.fetch.json_format);
-
-
-
-Create or replace test_table (JSON Variant);
-
-
-
-CREATE OR REPLACE STAGE Products.fetch.receipts_stage
-    URL = 's3://fetch-hiring/analytics-engineer/ineeddata-data-modeling/receipts.json.gz'
-    FILE_FORMAT = (type=json, compression=GZIP);
-
-Failure using stage area. Cause: [Access Denied (Status Code: 403; Error Code: AccessDenied)]
-
-
---Testing communication with IAM AWS instance
-SELECT SYSTEM$GET_AWS_S3_PRE_SIGNED_URL('s3://snowflake-docs/warehouse-provisioning.png'); 
-
-
-
-CREATE OR REPLACE FILE FORMAT my_json_format
-    TYPE = 'JSON'
-    COMPRESSION = 'GZIP'
-    STRIP_OUTER_ARRAY = FALSE
-    STRIP_NULL_VALUES = TRUE
-    IGNORE_UTF8_ERRORS = TRUE;
-
-
-CREATE OR REPLACE STAGE my_stage
-    URL = 's3://habeebanalytics/receipts.json'
-    FILE_FORMAT = (FORMAT_NAME = my_json_format);
-
-CREATE OR REPLACE TABLE receipts (
-    _id VARCHAR,
-    bonusPointsEarned NUMBER,
-    bonusPointsEarnedReason STRING,
-    createDate TIMESTAMP,
-    dateScanned TIMESTAMP,
-    finishedDate TIMESTAMP,
-    modifyDate TIMESTAMP,
-    pointsAwardedDate TIMESTAMP,
-    pointsEarned NUMBER,
-    purchaseDate TIMESTAMP,
-    purchasedItemCount NUMBER,
-    rewardsReceiptItemList VARIANT,
-    rewardsReceiptStatus STRING,
-    totalSpent NUMBER,
-    userId STRING
-);
-
-COPY INTO receipts
-FROM @my_stage/receipts.json.gz
-FILE_FORMAT = (FORMAT_NAME = my_json_format)
 MATCH_BY_COLUMN_NAME=CASE_SENSITIVE
 ON_ERROR = 'CONTINUE';
 
 
-
-CREATE OR REPLACE STAGE my_json_stage 
-    URL = 's3://habeebanalytics/receipts.json'; 
-
-
-
-
---Testing the s3 access
-
-create or replace stage my_s3_stage
-  url='s3://habeebanalytics/brands.json'
-  FILE_FORMAT = (TYPE = JSON);
-
-desc stage my_s3_stage;
-
-list @my_s3_stage;
-
-CREATE or replace FILE FORMAT my_json_format
-  TYPE = 'JSON'
-  STRIP_OUTER_ARRAY = TRUE;
-
-CREATE or replace TABLE brand_data (
-  _id STRING,
-  barcode STRING,
-  brandCode STRING,
-  category STRING,
-  categoryCode STRING,
-  cpg VARIANT, -- Assuming this could be a complex structure
-  topBrand BOOLEAN,
-  name STRING
-);
-
-CREATE or replace STAGE my_stage
-  URL = 's3://habeebanalytics'
-  FILE_FORMAT = (FORMAT_NAME = my_json_format);
-
-
-COPY INTO brand_data
-FROM @my_stage/brands.json
-FILE_FORMAT = (FORMAT_NAME = my_json_format)
-MATCH_BY_COLUMN_NAME = CASE_SENSITIVE
-ON_ERROR = 'CONTINUE';
+--Testing new code
+-- Populate the receipts with the data from the stage receipts
+COPY INTO Products.fetch.receipts (
+    _id,
+    bonusPointsEarned,
+    bonusPointsEarnedReason,
+    createDate,
+    dateScanned,
+    finishedDate,
+    modifyDate,
+    pointsAwardedDate,
+    pointsEarned,
+    purchaseDate,
+    purchasedItemCount,
+    rewardsReceiptItemList,
+    rewardsReceiptStatus,
+    totalSpent,
+    userId
+)
+FROM (
+    SELECT 
+        $1:_id:"$oid"::VARCHAR,
+        $1:bonusPointsEarned::FLOAT,
+        $1:bonusPointsEarnedReason::VARCHAR,
+        TO_TIMESTAMP_NTZ($1:createDate:"$date"::NUMBER / 1000),
+        TO_TIMESTAMP_NTZ($1:dateScanned:"$date"::NUMBER / 1000),
+        TO_TIMESTAMP_NTZ($1:finishedDate:"$date"::NUMBER / 1000),
+        TO_TIMESTAMP_NTZ($1:modifyDate:"$date"::NUMBER / 1000),
+        TO_TIMESTAMP_NTZ($1:pointsAwardedDate:"$date"::NUMBER / 1000),
+        $1:pointsEarned::FLOAT,
+        TO_TIMESTAMP_NTZ($1:purchaseDate:"$date"::NUMBER / 1000),
+        $1:purchasedItemCount::INT,
+        $1:rewardsReceiptItemList::VARIANT,
+        $1:rewardsReceiptStatus::VARCHAR,
+        $1:totalSpent::FLOAT,
+        $1:userId::VARCHAR
+    FROM @Products.fetch.receipts_stage
+) ON_ERROR = 'CONTINUE'
+FILE_FORMAT = (FORMAT_NAME = Products.fetch.json_format);
 
 
--- Create a staging table
-CREATE OR REPLACE TABLE brand_data_staging (raw VARIANT);
-
--- Copy data into the staging table
-COPY INTO brand_data_staging
-FROM @my_stage/brands.json
-FILE_FORMAT = (FORMAT_NAME = my_json_format)
-ON_ERROR = 'CONTINUE';
-
--- Insert data from the staging table into the structured table
-INSERT INTO brand_data
-SELECT 
-  raw:_id::STRING,
-  raw:barcode::STRING,
-  raw:brandCode::STRING,
-  raw:category::STRING,
-  raw:categoryCode::STRING,
-  raw:cpg,
-  raw:topBrand::BOOLEAN,
-  raw:name::STRING
-FROM brand_data_staging;
+select *
+from Products.fetch.receipts;
 
 
-CREATE FILE FORMAT my_csv_format
-  TYPE = 'CSV'
-  FIELD_OPTIONALLY_ENCLOSED_BY = '"'
-  SKIP_HEADER = 1;
+select *
+from Products.fetch.receipts;
 
-CREATE TABLE med_insured (
-  age INT,
-  sex VARCHAR,
-  bmi FLOAT,
-  children INT,
-  smoker VARCHAR,
-  region VARCHAR,
-  charges FLOAT
-);
+select *
+from Products.fetch.users;
 
 
+Select * from @products.fetch.receipts_stage (file_format=> 'Products.fetch.json_format');
 
-CREATE or replace stage my_s3_stage
-  URL='s3://habeebanalytics'
-  FILE_FORMAT = my_csv_format;
-
-
-copy into med_insured (age, sex, bmi, children, smoker, region, charges)
-from 's3://habeebanalytics/medical_insurance.csv'
-file_format = (
-    type = 'CSV'
-    field_delimiter = ','
-    skip_header = 1
-    );
-
-
--- Receipts table
-CREATE or replace TABLE receipts (
-  _id VARCHAR,
-  bonusPointsEarned FLOAT,
-  bonusPointsEarnedReason VARCHAR,
-  createDate TIMESTAMP_NTZ,
-  dateScanned TIMESTAMP_NTZ,
-  finishedDate TIMESTAMP_NTZ,
-  modifyDate TIMESTAMP_NTZ,
-  pointsAwardedDate TIMESTAMP_NTZ,
-  pointsEarned FLOAT,
-  purchaseDate TIMESTAMP_NTZ,
-  purchasedItemCount INT,
-  rewardsReceiptItemList VARIANT,
-  rewardsReceiptStatus VARCHAR,
-  totalSpent FLOAT,
-  userId VARCHAR
-);
-
--- Users table
-CREATE or replace TABLE users (
-  _id VARCHAR,
-  state VARCHAR,
-  createdDate TIMESTAMP_NTZ,
-  lastLogin TIMESTAMP_NTZ,
-  role VARCHAR,
-  active BOOLEAN
-);
-
--- Brands table
-CREATE or replace TABLE brands (
-  _id VARCHAR,
-  barcode VARCHAR,
-  brandCode VARCHAR,
-  category VARCHAR,
-  categoryCode VARCHAR,
-  cpg VARIANT,
-  topBrand BOOLEAN,
-  name VARCHAR
-);
-
-
--- Stage for users.json
-CREATE or replace STAGE users_stage
-  URL='s3://habeebanalytics/users.json'
-  FILE_FORMAT = (FORMAT_NAME = my_json_format);
-
--- Stage for brands.json
-CREATE or replace STAGE brands_stage
-  URL='s3://habeebanalytics/brands.json'
-  FILE_FORMAT = (FORMAT_NAME = my_json_format);
-
--- Stage for receipts.json
-CREATE or replace STAGE receipts_stage
-  URL='s3://habeebanalytics/receipts.json'
-  FILE_FORMAT = (FORMAT_NAME = my_json_format);
-
-
--- Load users data
-COPY INTO users
-FROM @users_stage
-FILE_FORMAT = (FORMAT_NAME = my_json_format)
-MATCH_BY_COLUMN_NAME=CASE_INSENSITIVE;
-
--- Load brands data
-COPY INTO brands
-FROM @brands_stage
-FILE_FORMAT = (FORMAT_NAME = my_json_format)
-MATCH_BY_COLUMN_NAME=CASE_INSENSITIVE;
-
--- Load receipts data
-COPY INTO receipts
-FROM @receipts_stage
-FILE_FORMAT = (FORMAT_NAME = my_json_format)
-MATCH_BY_COLUMN_NAME=CASE_INSENSITIVE;
-
-Create or replace stage my_s3_stage
-    url ='s3://habeebanalytics';
-
-desc stage my_s3_stage;
-
-list @my_s3_stage;
-
-select * 
-from @my_s3_stage/brands.json (file_format=>my_json_format);
-
-medical_insurance.csv
-shows file format in my_s3_stage;
